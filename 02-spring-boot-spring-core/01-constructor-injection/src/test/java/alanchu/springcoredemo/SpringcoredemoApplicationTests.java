6ce6f1ec01c32c7e3d398d69160fabf02d9f9340
Alan Chu <achu61@seas.upenn.edu>
2023-09-28 17:03:18 -0700

    added spring-boot core demo

---
+++ 02-spring-boot-spring-core/01-constructor-injection/src/test/java/alanchu/springcoredemo/SpringcoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package alanchu.springcoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
