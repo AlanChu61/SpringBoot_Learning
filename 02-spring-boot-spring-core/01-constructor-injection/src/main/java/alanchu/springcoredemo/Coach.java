e2640531e9ff654c668850c6ed159da6d89b9c8b
Alan Chu <achu61@seas.upenn.edu>
2023-09-28 17:17:55 -0700

    added Coach interface, CCoach Class and DemoController

---
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/alanchu/springcoredemo/Coach.java
@@ -0,0 +1,6 @@
+package alanchu.springcoredemo;
+
+public interface Coach {
+
+    String getDailyWorkout();
+}
---
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/alanchu/springcoredemo/DemoController.java
@@ -0,0 +1,22 @@
+package alanchu.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    // Constructor injection
+    @Autowired
+    public DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
